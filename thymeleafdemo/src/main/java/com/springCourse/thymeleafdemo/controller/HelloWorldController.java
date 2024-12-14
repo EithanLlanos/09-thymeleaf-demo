55568de093ffdcadfac391fe9639eedb324f40d1
Rubick <eithanllanosb@gmail.com>
2024-12-14 17:16:39 -0500

    Use postmapping instead of getmapping

--- thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.springCourse.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -36,7 +37,7 @@ public class HelloWorldController {
     }
 
     // need a controller method to read form data and add data to the model using @Requestparam binding
-    @RequestMapping("/processFormV3")
+    @PostMapping("/processFormV3")
     public String processFormV3(@RequestParam("studentName") String theName, Model model) {
 
         // convert the data to all caps
