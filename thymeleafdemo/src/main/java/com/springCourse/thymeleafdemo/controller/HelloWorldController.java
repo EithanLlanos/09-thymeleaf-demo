5605a0a8ac33031d5607078cfbbb0dab9e7b056d
Rubick <eithanllanosb@gmail.com>
2024-12-14 15:26:34 -0500

    Add model to the MVC

--- thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.springCourse.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,9 +15,22 @@ public class HelloWorldController {
     }
 
     // need a controller method to process the HTML form
-
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld-process";
+    }
+
+    // need a controller method to read form data and add data to the model
+    @RequestMapping("/processFormV2")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+        // convert that data to uppercase and create message
+        String msg = "Yo " + theName.toUpperCase();
+        // add message to the model
+        model.addAttribute("message", msg);
+
         return "helloworld-process";
     }
 }
