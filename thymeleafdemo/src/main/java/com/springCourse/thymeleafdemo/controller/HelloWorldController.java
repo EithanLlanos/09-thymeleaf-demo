63e269429876f1bc1e6462150ff066a3467c3a6e
Rubick <eithanllanosb@gmail.com>
2024-12-14 16:30:07 -0500

    Add a BETTER way to make a controller using parameter binding

--- thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -29,6 +30,22 @@ public class HelloWorldController {
         // convert that data to uppercase and create message
         String msg = "Yo " + theName.toUpperCase();
         // add message to the model
+        model.addAttribute("message", msg);
+
+        return "helloworld-process";
+    }
+
+    // need a controller method to read form data and add data to the model using @Requestparam binding
+    @RequestMapping("/processFormV3")
+    public String processFormV3(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String msg = "Hey, my friend V3!" + theName;
+
+        //add message to the model
         model.addAttribute("message", msg);
 
         return "helloworld-process";
