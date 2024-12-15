425d56ece6f2446dc5ad1c6bd5e27f7508274b2f
Rubick <eithanllanosb@gmail.com>
2024-12-15 12:58:53 -0500

    Set up Form data binding endpoint and htmls

--- thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/StudentController.java
+++ thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/StudentController.java
@@ -5,6 +5,8 @@ import com.springCourse.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -22,5 +24,12 @@ public class StudentController {
         theModel.addAttribute("student", theStudent);
 
         return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+
+        return "student-confirmation";
     }
 }
