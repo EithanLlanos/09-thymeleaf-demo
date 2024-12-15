a077edd4235ffaa5fad3001a3303c9cebab07f9a
Rubick <eithanllanosb@gmail.com>
2024-12-15 15:01:02 -0500

    Refactoring countries hardcode

--- thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/StudentController.java
+++ thymeleafdemo/src/main/java/com/springCourse/thymeleafdemo/controller/StudentController.java
@@ -2,15 +2,20 @@ package com.springCourse.thymeleafdemo.controller;
 
 
 import com.springCourse.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
 
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -22,6 +27,9 @@ public class StudentController {
         // add a student object to the model
 
         theModel.addAttribute("student", theStudent);
+
+        // add a country list to the model
+        theModel.addAttribute("countries", countries);
 
         return "student-form";
     }
